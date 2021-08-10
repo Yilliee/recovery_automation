0d6ed5896797bd05a4ec6b73416660f07cee08f1
Yillié <yilliee@protonmail.com>
2021-08-10 15:27:42 +0500

    Install an ssh client/server ; correct the username for a repo; add the commit ssh to cherry-pick

--- build.sh
+++ build.sh
@@ -4,17 +4,19 @@ echo ""
 echo ""
 echo "Making sure the required dependencies are there"
 echo ""
-apt update
+apt update --fix-missing
+apt install openssh-server -y
 apt install git-core gnupg flex bison build-essential \
 zip curl zlib1g-dev gcc-multilib g++-multilib \
 libc6-dev-i386 libncurses5-dev lib32ncurses5-dev \
 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev \
-libxml2-utils xsltproc unzip fontconfig openjdk-8-jdk
+libxml2-utils xsltproc unzip fontconfig openjdk-8-jdk -y
 echo "Installing the repo launcher"
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
+echo ""
 echo "Configuring git"
 git config --global user.name "Yillié"
 git config --global user.email "yilliee@protonmail.com"
@@ -25,8 +27,8 @@ echo "Syncing Ofox"
 git clone https://gitlab.com/orangefox/sync.git ; cd sync
 ./get_fox_10.sh ~/fox
 cd ~/fox/vendor/recovery
-git fetch https://gitlab.com/yilliee/vendor_recovery fox_10.0
-git cherry-pick <commit>
+git fetch https://gitlab.com/yillie/vendor_recovery fox_10.0
+git cherry-pick 76afae08593815f8053ce1d56a2f29856a7bda4c
 echo ""
 echo "Cloning trees"
 cd ~/fox
@@ -42,5 +44,4 @@ echo "Uploading zip"
 cd ~/fox/out/target/product/*
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet $(ls OrangeFox*.zip)
-
 
