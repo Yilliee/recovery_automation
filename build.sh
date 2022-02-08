51b9e6f9afade1f98c978cbb3691a18e1a4b6d05
Yillié <yilliee@protonmail.com>
2022-02-08 18:49:36 +0500

    build.sh : Do not repo sync twice
    The placement of empty files that required syncing twice
    before, should be fixed by now.

--- build.sh
+++ build.sh
@@ -32,7 +32,6 @@ mkdir ~/twrp-11
 cd ~/twrp-11
 repo init https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 --depth=1
 repo sync -j $(nproc --all)
-repo sync -j $(nproc --all)
 echo ""
 
 echo "Cloning trees"
