79737546bec1b1c55fb4fb0874e26a8bbecca670
Yillié <yilliee@protonmail.com>
2022-02-13 22:34:43 +0500

    build.sh : Skip all the patches to source + extra vars/files

--- build.sh
+++ build.sh
@@ -29,48 +29,20 @@ echo ""
 
 echo "Syncing Ofox"
 git clone https://gitlab.com/orangefox/sync.git ; cd sync
-wget http://transfer.sh/xO4UzS/0001-Fix-derps.patch
-git am < 0001-Fix-derps.patch
 ./get_fox_10.sh ~/fox
 cd ~/fox/vendor/recovery
 git fetch https://gitlab.com/yillie/vendor_recovery fox_10.0
 git cherry-pick 8212a5516cf9dece1f93cb3cafb6bcd69d261f7e
-git cherry-pick a5dee11a78e30787e1490c65ba7d49f0fbc0b791
-cd ~/fox/bootable/recovery
-git fetch https://github.com/Yilliee/fox_bootable_recovery 10.0_2
-git cherry-pick b2a046cefabf42c3a201622cc6560d138e0fbb32
 echo ""
 
 echo "Cloning trees"
 cd ~/fox
 git clone https://gitlab.com/orangefox/device/a51nsxx ~/fox/device/samsung/a51
-cd ~/fox/device/samsung/a51
-echo "" >> BoardConfig.mk
-echo "# Don't Include Apex in recovery image" >> BoardConfig.mk
-echo "TW_EXCLUDE_APEX := true" >> BoardConfig.mk
-cd ~/fox/device/samsung/a51/recovery/root
-rm init.recovery.usb.rc
-wget http://transfer.sh/BGQasM/init.recovery.usb.rc
 echo ""
 
 echo "Starting Build"
 cd ~/fox
 . build/envsetup.sh
-export OF_MAINTAINER="Yillié"
-unset FOX_DYNAMIC_SAMSUNG_FIX
-export FOX_CUSTOM_BINS_TO_INTERNAL="copy"
-export FOX_USE_SED_BINARY=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_GREP_BINARY=1
-export FOX_USE_XZ_UTILS=1
-export FOX_USE_NANO_EDITOR=1
-export FOX_REPLACE_BUSYBOX_PS=1
-export FOX_REPLACE_TOOLBOX_GETPROP=1
-export FOX_USE_BASH_SHELL=1
-export FOX_VERSION=R11.1_2
-export FOX_VARIANT=A12
-export FOX_ASH_IS_BASH=1
-export OF_STATUS_H="88"
 lunch omni_a51-eng
 make recoveryimage
 echo ""
