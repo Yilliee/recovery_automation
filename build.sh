84257c9116ddcf9471352da331ad881c2eeaa80c
Yillié <yilliee@protonmail.com>
2022-03-03 16:26:00 +0500

    Run all the tests in test.sh too

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "Executing Build Script"
 echo ""
 # Set Time zone for Tz-Data
 export TZ=Asia/Karachi
+export CIRRUS_WORKING_DIR="/drone/src"
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo ""
 echo "Making sure the required dependencies are there"
@@ -31,24 +32,6 @@ echo "Installed all the dependecies"
 echo ""
 echo ""
 
-echo "Syncing Octavi OS"
-mkdir Octavi && cd Octavi
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Octavi-OS-GSI/treble_manifest.git --depth 1 -b 12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || exit 2
-
-cd device/phh/treble && bash generate.sh octavi && cd ../../../ || exit 3
-
-# build rom
-source build/envsetup.sh
-lunch treble_arm64_bgN-userdebug
-export TZ=Asia/Karachi # Put before last build command
-make sepolicy
-make bootimage
-make init
-
-#mka systemimage
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-#rclone copy out/target/product/phhgsi_arm64_bgN/system.img cirrus:OctaviOS-GSI -P
+bash $CIRRUS_WORKING_DIR/test.sh
+bash $CIRRUS_WORKING_DIR/build_rom.sh
 
---
+++ build_rom.sh
@@ -0,0 +1,37 @@
+# sync rom
+export CIRRUS_WORKING_DIR="/drone/src"
+
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Yilliee/treble_manifest.git --depth 1 -b 12 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch octavi_arm64-bgN-userdebug
+export TZ=Asia/Dhaka #put before last build command
+#mka systemimage
+mka sepolicy
+mka init
+#mka
+
+if [ -d out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) ]; then
+	touch out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/system.img
+else
+	echo "out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) doesn't exist"
+	echo "Aborting..."
+	exit 2
+fi
+
+# Make a zip from the system image to upload
+zip out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/OctaviOS-12-arm64_bvN-$(TZ="Asia/Karachi" date +%Y%m%d).zip out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/system.img
+
+if [ -f out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip ]; then
+	echo "zip exists in the correct dir"
+	ls  $(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*zip
+	exit 0
+fi
+
+exit 3
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
