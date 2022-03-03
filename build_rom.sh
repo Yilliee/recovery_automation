352d1d38401a31198765ee1d345a6314dc1c6bac
Yillié <yilliee@protonmail.com>
2022-03-03 17:36:10 +0500

    Correct account name for local_manifest ; Skip branch name test ; Add missing part of test.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 export CIRRUS_WORKING_DIR="/drone/src"
 
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Yilliee/treble_manifest.git --depth 1 -b 12 .repo/local_manifests
+git clone https://github.com/Octavi-OS-GSI/treble_manifest.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
