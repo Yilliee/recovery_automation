30f8c1ed41155b96ba445143fd28d4303d024930
Yillié <yilliee@protonmail.com>
2022-06-06 15:02:49 +0500

    Clone m31s tree to build twrp

--- build.sh
+++ build.sh
@@ -32,29 +32,21 @@ mkdir ~/twrp-11
 cd ~/twrp-11
 repo init https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 --depth=1
 repo sync -j 20
-#cd ~/twrp-11/bootable/recovery
-#wget https://transfer.sh/b5yB7E/0001-1-2-Introduce-TW_CPU_POS_X-and-TW_TIME_POS_X.patch
-#git am 0001-1-2-Introduce-TW_CPU_POS_X-and-TW_TIME_POS_X.patch
-#cd ~/twrp-11/vendor/twrp
-#wget https://transfer.sh/uqutV0/0001-2-2-makevars-Introduce-TW_CPU_POS_X-and-TW_TIME_POS_.patch
-#git am 0001-2-2-makevars-Introduce-TW_CPU_POS_X-and-TW_TIME_POS_.patch
 cp /drone/src/ui.xml ~/twrp-11/bootable/recovery/gui/theme/portrait_hdpi/ui.xml || exit 1
 echo ""
 
 echo "Cloning trees"
 cd ~/twrp-11
-git clone https://github.com/Yilliee/recovery_a51 -b twrp-11 ~/twrp-11/device/samsung/a51 --depth=1 --single-branch
+git clone https://github.com/Yilliee/recovery_m31s -b twrp-11 ~/twrp-11/device/samsung/m31s --depth=1 --single-branch
 git clone https://github.com/Yilliee/recovery_universal9611-common -b twrp-11 ~/twrp-11/device/samsung/universal9611-common --depth=1 --single-branch
 #git clone https://github.com/Yilliee/android_kernel_samsung_exynos9611 -b Celicia ~/twrp-11/kernel/samsung/universal9610 --depth=1 --single-branch
-#echo "TW_CPU_POS_X := 590" >> ~/twrp-11/device/samsung/a51/BoardConfig.mk
-#echo "TW_TIME_POS_X := 120" >> ~/twrp-11/device/samsung/a51/BoardConfig.mk
 echo ""
 
 echo "Starting Build"
 cd ~/twrp-11
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch twrp_a51-eng
+lunch twrp_m31s-eng
 make recoveryimage -j$( nproc --all )
 echo ""
 
@@ -63,6 +55,6 @@ cd ~/twrp-11/out/target/product/*
 version=$(cat ~/twrp-11/bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 version=$(echo $version | cut -c1-5)
 
-mv recovery.img TWRP-11-${version}-a51-$(TZ='Asia/Karachi' date "+%Y%m%d-%H%M").img
+mv recovery.img TWRP-11-${version}-m31s-$(TZ='Asia/Karachi' date "+%Y%m%d-%H%M").img
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet $(ls TWRP*.img)
