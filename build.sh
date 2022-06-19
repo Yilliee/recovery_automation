9210eac6e97ce20d457afa51edfb7bd7a5a3182b
Yillié <yilliee@protonmail.com>
2022-06-19 10:51:19 +0500

    build.sh : Add -m31-f41 to the name of output image

--- build.sh
+++ build.sh
@@ -55,6 +55,6 @@ cd ~/twrp-11/out/target/product/*
 version=$(cat ~/twrp-11/bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 version=$(echo $version | cut -c1-5)
 
-mv recovery.img TWRP-11-${version}-m21-$(TZ='Asia/Karachi' date "+%Y%m%d-%H%M").img
+mv recovery.img TWRP-11-${version}-m21-m31-f41-$(TZ='Asia/Karachi' date "+%Y%m%d-%H%M").img
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet $(ls TWRP*.img)
